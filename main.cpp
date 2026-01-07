e0751c1b35bc232954307ab9de8e5204217351d9
Janmejai Pratap Tonk <devtonk24@gmail.com>
2026-01-07 12:52:02 +0530

    Add test output to program startup
    Added a 'Test' message to the console output at the start of the program for debugging or verification purposes.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
     cout << "Library Management System started." << endl;
+    cout << "Test" << endl;
     Library l;
     l.loadFromFile();
     l.displayBooks();
